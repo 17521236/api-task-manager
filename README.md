fbcc73dd8b8f0771a39d738cd0fe38b50fe2c097
TungNguyen <17521236@gm.uit.edu.vn>
2020-11-30 13:35:38 +0700

    update validate

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Api of task-manager
+# Task Manager Server
+
+Task Manager Server is server Api of [Task Manager](https://github.com/17521236/task-manager)
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to install and run.
+
+```bash
+npm install
+npm start
+```
+## Support
+If you like my work, feel free to:
+
+- ⭐ this repository. And we will be happy together :)
+
+Thanks a bunch for stopping by and supporting me!
+
+## Author
+
+- Nguyen Manh Tung - mtung199x@gmail.com
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,10 +2,35 @@ const User = require('../models/user.model')
 const md5 = require('md5')
 
 module.exports = {
-    create: (req, res) => {
-        req.body.password=md5(req.body.password);
-        User.create(req.body)
-            .then(value => res.send(value))
-            .catch(err => res.send(err))
+    create: async(req, res) => {
+        var email = req.body.email;
+        var password = req.body.password;
+        var name = req.body.name;
+        if (!email || email === '' || !password || !name) {
+            res.send({
+                result: '0',
+                message: 'Please complete all information'
+            })
+        }
+        // validate
+        var user = await User.find({ email: req.body.email })
+        if (user.length === 0) {
+            req.body.password = md5(req.body.password);
+            await User.create(req.body)
+                .then(value => res.send({
+                    result: '1',
+                    value: {...value }
+                }))
+                .catch(err => res.send({
+                    result: '0',
+                    message: 'Create user failed !',
+                    error: err
+                }))
+        } else {
+            res.send({
+                result: '0',
+                message: 'Email existed !'
+            })
+        }
     }
 }
